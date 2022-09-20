9d608d4f01622d67ff983c3db23d76daabbff93b
Tobias Ahrens <tobias.ahrens@zhinst.com>
2022-09-20 09:04:36 +0200

    Add constants to deviceutils reimport

--- src/zhinst/utils/shfqa/__init__.py
+++ src/zhinst/utils/shfqa/__init__.py
@@ -3,6 +3,9 @@ from zhinst.utils.shfqa.shfqa import *
 from zhinst.utils.shfqa import multistate
 
 __all__ = [
+    "SHFQA_MAX_SIGNAL_GENERATOR_WAVEFORM_LENGTH",
+    "SHFQA_MAX_SIGNAL_GENERATOR_CARRIER_COUNT",
+    "SHFQA_SAMPLING_FREQUENCY",
     "max_qubits_per_channel",
     "load_sequencer_program",
     "configure_scope",
@@ -13,5 +16,10 @@ __all__ = [
     "enable_scope",
     "configure_weighted_integration",
     "configure_result_logger_for_spectroscopy",
+    "configure_result_logger_for_readout",
+    "enable_result_logger",
+    "get_result_logger_data",
+    "configure_channel",
+    "configure_sequencer_triggering",
     "multistate",
 ]
